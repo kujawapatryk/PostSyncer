b6944e88983cf77deeda90278b0f58fd667d2b76
kujawapatryk <homik363@wp.pl>
2023-12-10 11:28:43 +0100

    Added setters and getters and Removed the 'lastname' column.

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -8,18 +8,15 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 class Post
 {
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
     // Author's name
-    #[ORM\Column(type: 'string', length: 100)]
+    #[ORM\Column(type: 'string', length: 255)]
     private string $name;
-
-    // Author's last name
-    #[ORM\Column(type:'string', length: 100)]
-    private string $lastName;
 
     // Title of the post
     #[ORM\Column(type: 'string', length: 255)]
@@ -33,4 +30,36 @@ class Post
     {
         return $this->id;
     }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setBody(string $body): void
+    {
+        $this->body = $body;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+
 }
